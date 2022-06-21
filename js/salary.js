94659533c3806daf68ec77335c74c059efd3e70b
angelayalaz <ayalazamorajoseangel@gmail.com>
2022-06-21 17:53:52 -0500

    finished calculating the median of colombia ficticial salary

---
+++ js/salary.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+    name:"juanito",
+    salary: 1000,
+});
+colombia.push({
+    name:"Tony",
+    salary: 2000,
+});
+colombia.push({
+    name:"geronimo",
+    salary: 1500,
+});
+colombia.push({
+    name:"Pepe",
+    salary: 2500,
+});
+colombia.push({
+    name:"forest",
+    salary: 900,
+});
+colombia.push({
+    name:"adan",
+    salary: 500,
+});
+colombia.push({
+    name:"Victor",
+    salary: 2300,
+});
+colombia.push({
+    name:"angel",
+    salary: 5000,
+});
+colombia.push({
+    name:"luna",
+    salary: 400,
+});
+colombia.push({
+    name: "Pedro",
+    salary: 500,
+});
+colombia.push({
+    name:"Pablo",
+    salary: 1600,
+});
+colombia.push({
+    name:"freya",
+    salary: 1200,
+});
+colombia.push({
+    name:"Ragnar",
+    salary: 2400,
+});
+colombia.push({
+    name:"Floki",
+    salary: 2450,
+});
+colombia.push({
+    name:"Magnus",
+    salary: 1900,
+});
+colombia.push({
+    name:"Lagertha",
+    salary: 3100,
+});
+colombia.push({
+    name:"Athelstan",
+    salary: 2700,
+});
+colombia.push({
+    name:"Donna",
+    salary: 2350,
+});
+colombia.push({
+    name:"Harvy",
+    salary: 4000,
+});
+colombia.push({
+    name:"Mike",
+    salary: 2100,
+});
